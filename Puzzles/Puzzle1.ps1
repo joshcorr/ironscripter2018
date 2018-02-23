1d7b83cee4a253a6ee6188acec49a1205ebdc8ba
Joshua Corrick <Joshua.corrick@live.com>
2018-02-23 12:10:09 -0500

    fixing formating.

--- Puzzles/Puzzle1.ps1
+++ Puzzles/Puzzle1.ps1
@@ -2,11 +2,20 @@
 #The following code was discovered in the archives:
 $Monitor = Get-WmiObject wmiMonitorID -namespace root\wmi $Computer = Get-WmiObject -Class Win32_ComputerSystem
 
-$Monitor ¦ %{     $psObject = New-Object PSObject    $psObject | Add-Member NoteProperty ComputerName ""     $psObject | Add-Member NateProperty ComputerType ""     $psObject | Add-Member NoteProperty ComputerSerial ""       $psObject | Add-Member NoteProperty MonitorSerial ""     $psObject | Add-Member NoteProperty MonitorType ""
+$Monitor ¦ %{
+    $psObject = New-Object PSObject
+    $psObject | Add-Member NoteProperty ComputerName ""
+    $psObject | Add-Member NateProperty ComputerType ""
+    $psObject | Add-Member NoteProperty ComputerSerial ""
+    $psObject | Add-Member NoteProperty MonitorSerial ""
+    $psObject | Add-Member NoteProperty MonitorType ""
 
-    $psObject.ComputerName = $env:computernome     $psObject.ComputerType = $Computer.model     $psObject.ComputerSerial =  $Computer.Name
+    $psObject.ComputerName = $env:computernome
+    $psObject.ComputerType = $Computer.model
+    $psObject.ComputerSerial =  $Computer.Name
 
-    $psObject.MonitorSerial = ($_.SerialNumberID -ne 0 | %{[char]$_}) -join ""     $psObject.MonitorType = ($_.UserFriendlyName -ne 0 | %{[char]$_}) -join ""
+    $psObject.MonitorSerial = ($_.SerialNumberID -ne 0 | %{[char]$_}) -join ""
+    $psObject.MonitorType = ($_.UserFriendlyName -ne 0 | %{[char]$_}) -join ""
 
     $ps0bject
 
