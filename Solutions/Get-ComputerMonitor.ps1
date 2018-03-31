1b9c04c7f576a5b3d56a26c99513e41a0c9968f8
Joshua Corrick <Joshua.corrick@live.com>
2018-03-30 23:50:52 -0400

    Mostly working Puzzle1

--- Solutions/Get-ComputerMonitor.ps1
+++ Solutions/Get-ComputerMonitor.ps1
@@ -11,54 +11,46 @@ function Get-ComputerMonitor{
 #>
     [CmdletBinding()]
     param(
+        [Parameter(
+            Mandatory=$false,
+            ValueFromPipeline=$true,
+            Position=0,
+            HelpMessage='Provide a string value of the computer you want to connect to')]
         [string[]]$computer
     )
     begin {
-        $Session = New-CimSession -ComputerName $Computer
-        $Monitors = Get-CimInstance -ClassName WmiMonitorID -namespace root\wmi
-        $Computer = Get-CimInstance -ClassName CIM_ComputerSystem -CimSession $Session
+        If($PSBoundParameters.ContainsKey('computer')){
+            $nodes = Get-CimInstance -ClassName CIM_ComputerSystem -ComputerName $computer
+        }
+        else{
+            $nodes = Get-CimInstance -ClassName CIM_ComputerSystem
+        }
+
     }
 
     process {
-        foreach($sesh in $Session){
-            $Computer = Get-CimInstance -ClassName CIM_ComputerSystem -CimSession $sesh
-            $Monitors = Get-CimInstance -ClassName WmiMonitorID -namespace root\wmi -CimSession $sesh
+        foreach($node in $nodes){
+            If($PSBoundParameters.ContainsKey('computer')){
+                $monitors = Get-CimInstance -ClassName WmiMonitorID -namespace root\wmi -ComputerName $node
+            }
+            else{
+                $monitors = Get-CimInstance -ClassName WmiMonitorID -namespace root\wmi
+            }
 
-        }
-        foreach($monitor in $Monitors){
-            [PSCustomObject]@{
-                "ComputerName" = $monitor.ComputerName;
-                "ComputerType" = $monitor.ComputerType;
-                "ComputerSerial" = $monitor.ComputerSerial;
-                "MonitorSerial" = $monitor.MonitorSerial;
-                "MonitorType" = $monitor.MonitorType
+            foreach($monitor in $monitors){
+                [PSCustomObject]@{
+                    "ComputerName" = $node.Name;
+                    "ComputerType" = $node.Model;
+                    "ComputerSerial" = $node.name;
+                    "MonitorSerial" = ($monitor.SerialNumberID -ne 0 | ForEach-Object {[char]$_}) -join "";
+                    "MonitorType" = if($null -ne $monitor.UserFriendlyName){($monitor.UserFriendlyName -ne 0 | ForEach-Object {[char]$_}) -join "";}
+                }
             }
 
         }
     }
 
     end {
-    }
-
-$Monitor = Get-WmiObject wmiMonitorID -namespace root\wmi
-$Computer = Get-WmiObject -Class Win32_ComputerSystem
-
-
-$Monitor | % {
-    $psObject = New-Object PSObject
-    $psObject | Add-Member -MemberType NoteProperty  -Name ComputerName -Value $PSItem.ComputerName
-    $psObject | Add-Member -MemberType NoteProperty -Name ComputerType -Value $PSItem.ComputerType
-    $psObject | Add-Member -MemberType NoteProperty -Name ComputerSerial -Value $PSItem.ComputerSerial
-    $psObject | Add-Member -MemberType NoteProperty -Name MonitorSerial -Value $PSItem.MonitorSerial
-    $psObject | Add-Member -MemberType NoteProperty -Name MonitorType -Value $PSItem.MonitorType
-
-    $psObject.ComputerName = $env:COMPUTERNAME
-    $psObject.ComputerType = $Computer.model
-    $psObject.ComputerSerial =  $Computer.Name
-
-    $psObject.MonitorSerial = ($_.SerialNumberID -ne 0 | %{[char]$_}) -join ""
-    $psObject.MonitorType = ($_.UserFriendlyName -ne 0 | %{[char]$_}) -join ""
-
-    $ps0bject
+        #Get-CimSession -ComputerName $Session.ComputerName | Remove-CimSession
     }
 }
